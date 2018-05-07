2986b03a347760da1a81f088e5f3ee3359a198f4
jinping.wjp <jinping.wjp@alibaba-inc.com>
2018-05-07 20:07:11 +0800

    feat: add vue rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "root": true,
+
+  "extends": [
+    "./index",
+  ].map(require.resolve),
+
+  "env": {
+    "node": true,
+  },
+
+  // 针对规则文件单独处理
+  "overrides": [{
+    "files": [
+      "index.js",
+      "rules/*.js",
+    ],
+    "rules": {
+      // 为了尽可能兼容 JSON 格式，统一使用双引号，且配置文件中的 key 一律加引号
+      "quote-props": [2, "always"],
+      "quotes": [2, "double"],
+
+      // 在配置规则时，数组的第一项为规则提示级别，为了与后边的配置区分，此项与规则名同行，所以关闭相关检测
+      "array-bracket-newline": 0,
+      "array-element-newline": 0,
+    },
+  }, ],
+}
